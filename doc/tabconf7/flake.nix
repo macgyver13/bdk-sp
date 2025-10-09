01c3caedf4e1a29182580cffef7f099925c35b91
nymius <155548262+nymius@users.noreply.github.com>
2025-10-09 18:38:16 +0200

    build(tabconf7): self reference sp-cli2 flake input

--- doc/tabconf7/flake.nix
+++ doc/tabconf7/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    sp-cli2.url = "github:nymius/bdk-sp/docs/workshops";
+    sp-cli2.url = "../..";
     bdk-cli.url = "github:nymius/bdk-cli/feat/nix-env";
   };
   outputs = {
