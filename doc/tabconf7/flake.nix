c0a6b2b3ec5f491a0e43cd6456edce281ce38be3
macgyver13 <4712150+macgyver13@users.noreply.github.com>
2025-10-11 18:13:34 +0200

    build(tabconf7): replace nixos unstable with nixos-24.05

--- doc/tabconf7/flake.nix
+++ doc/tabconf7/flake.nix
@@ -2,7 +2,7 @@
   description = "BDK Silent Payments Workshop Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     sp-cli2.url = "../..";
     bdk-cli.url = "github:nymius/bdk-cli/feat/nix-env";
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Dev environment for bdk_sp workspace";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
