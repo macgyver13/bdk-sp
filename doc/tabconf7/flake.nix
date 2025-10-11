bbc2e5e3f075ac5e3c61b43042e2b63613f188cf
nymius <155548262+nymius@users.noreply.github.com>
2025-10-11 12:29:59 -0400

    build(tabconf7): auto format flakes with Alejandra

--- doc/tabconf7/flake.nix
+++ doc/tabconf7/flake.nix
@@ -70,97 +70,97 @@
             sp-cli2.packages.${system}.sp-cli2
           ];
           shellHook = ''
-            export PS1="$ "
+                        export PS1="$ "
 
-            export BITCOIN_DATA_DIR="$PWD/.bitcoin"
-            mkdir -p "$BITCOIN_DATA_DIR"
+                        export BITCOIN_DATA_DIR="$PWD/.bitcoin"
+                        mkdir -p "$BITCOIN_DATA_DIR"
 
-            export BDK_DATA_DIR="$PWD/.bdk"
-            mkdir -p "$BDK_DATA_DIR"
+                        export BDK_DATA_DIR="$PWD/.bdk"
+                        mkdir -p "$BDK_DATA_DIR"
 
-            bitcoind -daemonwait -signet -datadir=$BITCOIN_DATA_DIR -daemonwait -txindex -blockfilterindex -peerblockfilters
+                        bitcoind -daemonwait -signet -datadir=$BITCOIN_DATA_DIR -daemonwait -txindex -blockfilterindex -peerblockfilters
 
-            export EXTRA_SCRIPTS="$PWD/.bin"
-            mkdir -p $EXTRA_SCRIPTS
+                        export EXTRA_SCRIPTS="$PWD/.bin"
+                        mkdir -p $EXTRA_SCRIPTS
 
-            cat > "$EXTRA_SCRIPTS/signet-cli" <<'EOF'
-#!/usr/bin/env bash
-bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet $@
-EOF
+                        cat > "$EXTRA_SCRIPTS/signet-cli" <<'EOF'
+            #!/usr/bin/env bash
+            bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet $@
+            EOF
 
-            chmod +x "$EXTRA_SCRIPTS/signet-cli"
+                        chmod +x "$EXTRA_SCRIPTS/signet-cli"
 
-            cat > "$EXTRA_SCRIPTS/signet-bdk" <<'EOF'
-#!/usr/bin/env bash
-bdk-cli --datadir "$BDK_DATA_DIR" --network signet wallet -w signet -e "$EXT_DESCRIPTOR" -i "$INT_DESCRIPTOR" -c rpc -u http://localhost:38332/ "$BITCOIN_DATA_DIR/signet/.cookie" -d sqlite "$@"
-EOF
+                        cat > "$EXTRA_SCRIPTS/signet-bdk" <<'EOF'
+            #!/usr/bin/env bash
+            bdk-cli --datadir "$BDK_DATA_DIR" --network signet wallet -w signet -e "$EXT_DESCRIPTOR" -i "$INT_DESCRIPTOR" -c rpc -u http://localhost:38332/ "$BITCOIN_DATA_DIR/signet/.cookie" -d sqlite "$@"
+            EOF
 
-            chmod +x "$EXTRA_SCRIPTS/signet-bdk"
+                        chmod +x "$EXTRA_SCRIPTS/signet-bdk"
 
-            cat > "$EXTRA_SCRIPTS/signet-sp" <<'EOF'
-#!/usr/bin/env bash
-sp-cli2 "$@"
-EOF
+                        cat > "$EXTRA_SCRIPTS/signet-sp" <<'EOF'
+            #!/usr/bin/env bash
+            sp-cli2 "$@"
+            EOF
 
-            chmod +x "$EXTRA_SCRIPTS/signet-sp"
+                        chmod +x "$EXTRA_SCRIPTS/signet-sp"
 
-            cat > "$EXTRA_SCRIPTS/regtest-cli" <<'EOF'
-#!/usr/bin/env bash
-just cli "$@"
-EOF
+                        cat > "$EXTRA_SCRIPTS/regtest-cli" <<'EOF'
+            #!/usr/bin/env bash
+            just cli "$@"
+            EOF
 
-            chmod +x "$EXTRA_SCRIPTS/regtest-cli"
+                        chmod +x "$EXTRA_SCRIPTS/regtest-cli"
 
-            cat > "$EXTRA_SCRIPTS/regtest-bdk" <<'EOF'
-#!/usr/bin/env bash
-just regtest-bdk "$@"
-EOF
+                        cat > "$EXTRA_SCRIPTS/regtest-bdk" <<'EOF'
+            #!/usr/bin/env bash
+            just regtest-bdk "$@"
+            EOF
 
-            chmod +x "$EXTRA_SCRIPTS/regtest-bdk"
+                        chmod +x "$EXTRA_SCRIPTS/regtest-bdk"
 
-            cat > "$EXTRA_SCRIPTS/regtest-sp" <<'EOF'
-#!/usr/bin/env bash
-just regtest-sp "$@"
-EOF
+                        cat > "$EXTRA_SCRIPTS/regtest-sp" <<'EOF'
+            #!/usr/bin/env bash
+            just regtest-sp "$@"
+            EOF
 
-            chmod +x "$EXTRA_SCRIPTS/regtest-sp"
+                        chmod +x "$EXTRA_SCRIPTS/regtest-sp"
 
-            export PATH="$EXTRA_SCRIPTS:$PATH"
+                        export PATH="$EXTRA_SCRIPTS:$PATH"
 
-            export RPC_USER="__cookie__"
-            export RPC_PASS=$(cat $BITCOIN_DATA_DIR/signet/.cookie | cut -d ':' -f2)
-            export RPC_URL="http://127.0.0.1:38332/"
+                        export RPC_USER="__cookie__"
+                        export RPC_PASS=$(cat $BITCOIN_DATA_DIR/signet/.cookie | cut -d ':' -f2)
+                        export RPC_URL="http://127.0.0.1:38332/"
 
-            if [ ! -f "$BDK_DATA_DIR/.external_descriptor" ] || [ ! -f "$BDK_DATA_DIR/.internal_descriptor" ]; then
-              rm -rf $BDK_DATA_DIR/signet
-              rm -rf $BDK_DATA_DIR/regtest
-              XPRV=$(bdk-cli --datadir $BDK_DATA_DIR --network signet key generate | jq -r '.xprv')
-              echo "tr($XPRV/86h/1h/0h/0/*)" > "$BDK_DATA_DIR/.external_descriptor"
-              echo "tr($XPRV/86h/1h/0h/1/*)" > "$BDK_DATA_DIR/.internal_descriptor"
-            fi
+                        if [ ! -f "$BDK_DATA_DIR/.external_descriptor" ] || [ ! -f "$BDK_DATA_DIR/.internal_descriptor" ]; then
+                          rm -rf $BDK_DATA_DIR/signet
+                          rm -rf $BDK_DATA_DIR/regtest
+                          XPRV=$(bdk-cli --datadir $BDK_DATA_DIR --network signet key generate | jq -r '.xprv')
+                          echo "tr($XPRV/86h/1h/0h/0/*)" > "$BDK_DATA_DIR/.external_descriptor"
+                          echo "tr($XPRV/86h/1h/0h/1/*)" > "$BDK_DATA_DIR/.internal_descriptor"
+                        fi
 
-            if [ ! -f ".tr_xprv" ]; then
-              BLOCKCHAININFO=$(bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet getblockchaininfo)
-              HEIGHT=$(echo $BLOCKCHAININFO | jq -r '.blocks')
-              HASH=$(echo $BLOCKCHAININFO | jq -r '.bestblockhash')
-              sp-cli2 create --network signet --birthday-height $HEIGHT --birthday-hash $HASH | jq -r '.tr_xprv' > ".tr_xprv"
-            fi
+                        if [ ! -f ".tr_xprv" ]; then
+                          BLOCKCHAININFO=$(bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet getblockchaininfo)
+                          HEIGHT=$(echo $BLOCKCHAININFO | jq -r '.blocks')
+                          HASH=$(echo $BLOCKCHAININFO | jq -r '.bestblockhash')
+                          sp-cli2 create --network signet --birthday-height $HEIGHT --birthday-hash $HASH | jq -r '.tr_xprv' > ".tr_xprv"
+                        fi
 
-            # Start Regtest node on VM machine
-            just init
+                        # Start Regtest node on VM machine
+                        just init
 
-            export TR_XPRV=$(cat ".tr_xprv")
-            export EXT_DESCRIPTOR=$(cat "$BDK_DATA_DIR/.external_descriptor")
-            export INT_DESCRIPTOR=$(cat "$BDK_DATA_DIR/.internal_descriptor")
-            export EXTRA_PEER=$(bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet getpeerinfo | jq -r 'map(select(.servicesnames[] | contains ("COMPACT"))) | .[0].addr' | cut -sd ":" -f1)
+                        export TR_XPRV=$(cat ".tr_xprv")
+                        export EXT_DESCRIPTOR=$(cat "$BDK_DATA_DIR/.external_descriptor")
+                        export INT_DESCRIPTOR=$(cat "$BDK_DATA_DIR/.internal_descriptor")
+                        export EXTRA_PEER=$(bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet getpeerinfo | jq -r 'map(select(.servicesnames[] | contains ("COMPACT"))) | .[0].addr' | cut -sd ":" -f1)
 
-            trap "bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet stop && just stop" EXIT
+                        trap "bitcoin-cli --datadir=$BITCOIN_DATA_DIR --chain=signet stop && just stop" EXIT
 
-            if [[ -z "''${EXTRA_PEER}" ]]; then
-              echo "No compact block filter peers found. Ask for one."
-            else
-              echo "Compact block filter peer available: $EXTRA_PEER";
-            fi
+                        if [[ -z "''${EXTRA_PEER}" ]]; then
+                          echo "No compact block filter peers found. Ask for one."
+                        else
+                          echo "Compact block filter peer available: $EXTRA_PEER";
+                        fi
           '';
         };
       }
--- flake.nix
+++ flake.nix
@@ -37,24 +37,24 @@
               pname = "sp-cli2";
               version = "0.1.0";
 
-            # Disable cargo-auditable which doesn't support edition 2024
-            auditable = false;
-            src = ./.;
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              outputHashes = {
-                "bdk_tx-0.1.0" = "sha256-kND3yQ80ld8bQskf1Y+aDgJnZegTEviVV9d2zc7wwuQ=";
+              # Disable cargo-auditable which doesn't support edition 2024
+              auditable = false;
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "bdk_tx-0.1.0" = "sha256-kND3yQ80ld8bQskf1Y+aDgJnZegTEviVV9d2zc7wwuQ=";
+                };
+              };
+
+              buildAndTestSubdir = "cli/v2";
+              meta = with pkgs.lib; {
+                description = "A lightweight command line bitcoin silent payment wallet powered by BDK";
+                homepage = "https://bitcoindevkit.org";
+                license = with licenses; [mit];
+                mainProgram = "sp-cli2";
               };
             };
-
-            buildAndTestSubdir = "cli/v2";
-            meta = with pkgs.lib; {
-              description = "A lightweight command line bitcoin silent payment wallet powered by BDK";
-              homepage = "https://bitcoindevkit.org";
-              license = with licenses; [mit];
-              mainProgram = "sp-cli2";
-            };
-          };
         };
 
         defaultPackage = self.packages.${system}.sp-cli2;
